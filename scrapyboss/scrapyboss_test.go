eca3efe233943ac12720bc9e6cfd77f18ae8163e
Stijn Vorstenbosch <vorstenbosch.stijn@gmail.com>
2021-03-28 21:53:00 +0200

    added test

--- scrapyboss/scrapyboss_test.go
+++ scrapyboss/scrapyboss_test.go
@@ -70,6 +70,42 @@ func TestScrapyBoss(t *testing.T) {
 	}
 }
 
+func TestScrapyBossIterations(t *testing.T) {
+	// Given
+	config := ScrapyBossConfig{
+		ScrapeIntervalInSeconds: 5,
+		ScrapeEndpoints: []ScrapeEndpoint{
+			ScrapeEndpoint{
+				Endpoint: "http://localhost:5555",
+				Selectors: []scrapy.Selector{
+					scrapy.Selector{
+						Name:           "test",
+						Value:          "//div",
+						TypeOfSelector: "xpath",
+					},
+				},
+			},
+		},
+	}
+
+	scrapyBoss := NewScrapyBoss(config)
+
+	// When
+	scrapyBoss.Start()
+	defer scrapyBoss.Stop()
+
+	time.Sleep(15 * time.Second)
+
+	// Then
+	if scrapyBoss.GetConfig().ScrapeIntervalInSeconds != 5 {
+		t.Errorf("Config 'ScrapeIntervalInSeconds' was not '5', as expected, but was '%v'", scrapyBoss.GetConfig().ScrapeIntervalInSeconds)
+	}
+
+	if len(scrapyBoss.GetScrapeData()) > 1 {
+		t.Errorf("Did not found multiple scrape results")
+	}
+}
+
 func setup() {
 	server := &http.Server{Addr: ":5555"}
 
